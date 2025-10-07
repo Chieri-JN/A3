bc98c26f0bc74511197dc4448f88432d34d080f8
Chieri-JN <115610242+Chieri-JN@users.noreply.github.com>
2025-10-06 23:08:01 -0400

    partially complete with part 1

--- src/lib/AQIChart.svelte
+++ src/lib/AQIChart.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	// import { timeStamp } from 'console';
 	import * as d3 from 'd3';
+	import { onMount } from 'svelte';
 
 	interface Item {
 		city: string;
@@ -12,19 +14,142 @@
 		usAqi: number;
 	}
 
+	interface dataPoint {
+		tp : Date;
+		usAqi: number
+	}
+
 	// properties this component accepts
-	const { data }: { data: Item[] } = $props();
+	let { data , url, showRawData=false}: { data: Item[] , url : string, showRawData: Boolean} = $props();
+	let colours = [
+		{"name":"Good","min":0,"max":50,"color":"#9cd84e"},
+		{"name":"Moderate","min":51,"max":100,"color":"#facf39"},
+		{"name":"Unhealthy for Sensitive Groups","min":101,"max":150,"color":"#f99049"},
+		{"name":"Unhealthy","min":151,"max":200,"color":"#f65e5f"},
+		{"name":"Very Unhealthy","min":201,"max":300,"color":"#a070b6"},
+		{"name":"Hazardous","min":301,"color":"#a06a7b"},
+		{"name":"None Data","min":null,"color":"#bbbbbb"}
+	]
+	console.log(data);
+	let minYear = data.reduce((z, a) => z < a.timestamp ? z : a.timestamp , data[0].timestamp || new Date());
+	let maxYear = data.reduce((z, a) => z > a.timestamp ? z : a.timestamp, data[0].timestamp || new Date());
+
+	let minAQI = data.reduce((z, a) => z < a.usAqi ? z : a.usAqi , data[0].usAqi);
+	let maxAQI = data.reduce((z, a) => z > a.usAqi ? z : a.usAqi, data[0].usAqi);
+
+	function getColour(val : number) {
+		if (0 <= val && val <= 50 ) {
+			return colours[0].color;
+		} else if (51 <= val && val <= 100 ){
+			return colours[1].color;
+		} else if (101 <= val && val <= 150 ) {
+			return colours[2].color;
+		} else if (151 <= val && val <= 200 ) {
+			return colours[3].color;		
+		} else if (201 <= val && val <= 300 ) {
+			return colours[4].color;
+		} else if (301 <= val ) {
+			return colours[5].color;
+		} else {
+			return colours[6].color;
+		}
+	}
+
+	let symbolGenerator = d3.symbol().type(d3.symbolCircle).size(20);
+	let pointData = data.map(d => {
+		const dp : dataPoint = {
+			tp : d.timestamp,
+			usAqi : d.usAqi
+		} 
+		return dp;
+	})
+	let points = pointData.map(d => symbolGenerator(d));
+	// let pointGenerator = d3.
+	// let symbolData = 
+
+	let svgEl: SVGSVGElement;
+	function makeGraph() {
+		const margin = {top: 10, right: 30, bottom: 30, left: 60},
+			width = 600 - margin.left - margin.right,
+			height = 400 - margin.top - margin.bottom;
+
+		const svg = d3.select(svgEl)
+			.append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append("g")
+			.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+		// X axis
+		const x = d3.scaleTime()
+			.domain([minYear,maxYear])
+			.range([0, width]);
+		svg.append("g")
+			.attr("transform", `translate(0, ${height})`)
+			.call(d3.axisBottom(x).ticks(5));
+
+		const y = d3.scaleLinear()
+			.domain([0, maxAQI])
+			.range([ height, 0]);
+		svg.append("g")
+			.call(d3.axisLeft(y));
+
+		svg.append('g')
+			.selectAll("dot")
+			.data(data)
+			.join("circle")
+			.attr("cx", function (d) { return x(d.timestamp); } )
+			.attr("cy", function (d) { return y(d.usAqi); } )
+			.attr("r", 1.5)
+			.style("fill",  "#000")
+			// .style("fill",  d => getColour(d.usAqi))
+
+	}
+
+	onMount(makeGraph);
 
 	// just for debugging; can be removed
 	$inspect(data);
+
+
 </script>
 
-<pre>
-{JSON.stringify(data[0], null, 2)}
-</pre>
+<!--<pre>-->
+<!--{JSON.stringify(data[0], null, 2)}-->
+<!--</pre>-->
+{#if showRawData}
+<!--	<p>Data url {url}</p>-->
+{/if}
+
+<svg bind:this={svgEl} width="600" height="320"  style:display={showRawData ? 'block' : 'none'}>
+</svg>
+<!--<svg viewBox="-50 -50 100 100">-->
+	<!-- {#each points as p, i}  -->
+<!--<svg id="my_dataviz">-->
+<!--{#if showRawData}-->
+
+<!--{/if} -->
+	<!--{#each data as dp}]-->
+	<!--		{#if showRawData}-->
+	<!--		<path d={dp.timestamp.getFullYear().toString()} fill={getColour(dp.usAqi)}/>-->
+	<!--	&lt;!&ndash; <path d={dp} fill={getColour(dp.usAqi)}/> &ndash;&gt;-->
+	<!--		{/if}-->
+	<!--{/each}-->
+
+<p>svg</p>
 
 <style>
 	svg {
 		font-family: sans-serif;
+		overflow: visible;
+		.legend span {
+			aspect-ratio: 1 / 1;
+			background: var(--color);
+		}
+	}
+
+	legend span {
+		aspect-ratio: 1 / 1;
+		background: var(--color);
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,37 +3,139 @@
 	import * as d3 from 'd3';
 
 	const datasets = {
-		avalon: 'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BAvalon%5D_daily-avg.csv',
+		avalon:
+			['https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BAvalon%5D_daily-avg.csv'],
 		glassport_high_street:
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BGlassport%20High%20Street%5D_daily-avg.csv',
+			['https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BGlassport%20High%20Street%5D_daily-avg.csv'],
 		lawrenceville:
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BLawrenceville%5D_daily-avg.csv',
+			['https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BLawrenceville%5D_daily-avg.csv'],
 		liberty_sahs:
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BLiberty%20(SAHS)%5D_daily-avg.csv',
+			['https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BLiberty%20(SAHS)%5D_daily-avg.csv'],
 		manchester:
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BManchester%5D_daily-avg.csv',
+			['https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BManchester%5D_daily-avg.csv'],
 		north_braddock:
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BNorth%20Braddock%5D_daily-avg.csv',
+			['https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BNorth%20Braddock%5D_daily-avg.csv'],
 		parkway_east_near_road:
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BParkway%20East%20(Near%20Road)%5D_daily-avg.csv',
+			['https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BParkway%20East%20(Near%20Road)%5D_daily-avg.csv'],
 		usa_pennsylvania_pittsburgh:
-			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BUSA-Pennsylvania-Pittsburgh%5D_daily-avg.csv'
+			['https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BUSA-Pennsylvania-Pittsburgh%5D_daily-avg.csv'],
+
+		// Combine all the csv
+		all : [
+			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BAvalon%5D_daily-avg.csv',
+			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BGlassport%20High%20Street%5D_daily-avg.csv',
+			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BLawrenceville%5D_daily-avg.csv',
+			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BLiberty%20(SAHS)%5D_daily-avg.csv',
+			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BManchester%5D_daily-avg.csv',
+			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BNorth%20Braddock%5D_daily-avg.csv',
+			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BParkway%20East%20(Near%20Road)%5D_daily-avg.csv',
+			'https://dig.cmu.edu/datavis-fall-2025/assignments/data/%5BUSA-Pennsylvania-Pittsburgh%5D_daily-avg.csv',
+		]
 	};
 
-	const selectedDataset: keyof typeof datasets = $state('avalon');
+	let showRawData = $state(false);
 
-	const data = $derived.by(() =>
-		d3.csv(datasets[selectedDataset], (d: any) => ({
-			city: d.City,
-			country: d.Country,
-			mainPollutant: d['Main pollutant'],
-			pm25: +d['PM2.5'],
-			state: d.State,
-			stationName: d['Station name'],
-			timestamp: new Date(d['Timestamp(UTC)']),
-			usAqi: +d['US AQI']
-		}))
+	let selectedDataset: keyof typeof datasets = $state('avalon');
+
+	const data = $derived.by(async () =>
+	{
+		if (selectedDataset === "all") {
+			const combinedData = datasets["all"].map(dataSet => {
+					return d3.csv(dataSet, (d: any) =>
+						({
+								city: d.City,
+								country: d.Country,
+								mainPollutant: d['Main pollutant'],
+								pm25: +d['PM2.5'],
+								state: d.State,
+								stationName: d['Station name'],
+								timestamp: new Date(d['Timestamp(UTC)']),
+								usAqi: +d['US AQI']
+							}
+						))
+
+			});
+			const loadedData = await Promise.all(combinedData);
+			return loadedData.flat();
+
+		} else {
+			return d3.csv(datasets[selectedDataset][0], (d: any) => ({
+					city: d.City,
+					country: d.Country,
+					mainPollutant: d['Main pollutant'],
+					pm25: +d['PM2.5'],
+					state: d.State,
+					stationName: d['Station name'],
+					timestamp: new Date(d['Timestamp(UTC)']),
+					usAqi: +d['US AQI']
+				}
+			))
+		}
+	}
 	);
+
+	// const data = $derived.by(() =>
+	// 	d3.csv(datasets[selectedDataset], (dt: any[]) => ({
+	// 			if (dt.Lenght() == 1) {
+	// 				let d = dt[0];
+	// 				return {
+	// 					city: d.City,
+	// 					country: d.Country,
+	// 					mainPollutant: d['Main pollutant'],
+	// 					pm25: +d['PM2.5'],
+	// 					state: d.State,
+	// 					stationName: d['Station name'],
+	// 					timestamp: new Date(d['Timestamp(UTC)']),
+	// 					usAqi: +d['US AQI']
+	// 				}
+	// 			} else {
+	//
+	// 			}
+	// 		}
+	// 	))
+	// );
+
+
+	const stations = [
+		{ 
+			name : 'Avalon' , 
+			id : "avalon"
+		},
+		{
+			name :"Glassport High Street",
+			id : "glassport_high_street"
+		},
+		{ 
+			name :"North Braddock", 
+			id : 'north_braddock'
+		},
+		{ 
+			name :"Lawrenceville", 
+			id :  "lawrenceville"
+		},
+		{ 
+			name :"Parkway East", 
+			id : "parkway_east_near_road" 
+		},
+		{ 
+			name :"Manchester", 
+			id : "manchester" 
+		},
+		{ 
+			name :"Liberty (SAHS)" , 
+			id : "liberty_sahs"
+		},
+		{ 
+			name : "USA Pennsylvania Pittsburgh",
+			id : "usa_pennsylvania_pittsburgh"
+		},
+		{
+			name : "all",
+			id : "all"
+		}
+
+	];
+	stations.sort((a, b) => a.name.localeCompare(b.name));
 </script>
 
 {#await data}
@@ -41,15 +143,46 @@
 	<p>loading data...</p>
 {:then data}
 	<!-- promise was fulfilled or not a Promise -->
-	<h2>AQI Chart</h2>
-	<AQIChart {data} />
+	<h2>AQI Chart for {data[0].stationName || data[0].city}</h2>
+	<div>
+		<label class="region-selector">
+			<select class="region-selector" bind:value={selectedDataset}>
+				<option value = "all">--No region Selected (show all) --</option>
+				{#each stations as s}
+					<option value ={s.id}>
+						{s.name}
+						{ data.length} 
+					</option>
+				{/each}
+			</select>
+		</label>
+		
+		<div>
+			<label for="ShowRawData">
+				Show Raw Data
+				<input type="checkbox" id="ShowRawData" bind:checked={showRawData}>
+			</label>
+		</div>
+		<p>Number of Records { data.length} </p>
+	</div>
+
+	<div>
+		Visualization here
+		<AQIChart {data} url={datasets[selectedDataset]} showRawData={showRawData} />
+	</div>
+	
 {:catch error}
 	<!-- promise was rejected -->
 	<p>Something went wrong: {error.message}</p>
 {/await}
 
+
 <style>
 	* {
 		font-family: sans-serif;
+	}
+
+	.region-selector{
+		align-self: center;
 	}
 </style>
